e8e980f1997ff375b6346d2f63273d5b8c27dbfc
rebwang <rebwang20@gmail.com>
2025-03-13 10:44:32 -0700

    adding comment to SimpleXMLReader

--- Module07/SimpleXMLReader.java
+++ Module07/SimpleXMLReader.java
@@ -9,6 +9,7 @@ import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.Attributes;
 
 public class SimpleXMLReader {
+    // Reads an XML file at the specified filePath and parses it using SAX.
     public void read(String filePath) {
         try {
             File inputFile = new File(filePath);
@@ -24,16 +25,19 @@ public class SimpleXMLReader {
         }
     }
 
+    // This is a nested class
+    // This class extends DefaultHandler and defines how SAX processes the XML elements.
     static class SimpleXMLHandler extends DefaultHandler {
         List<Person> people;
         Map<String, String> current;
         StringBuffer buffer = new StringBuffer();
 
+        // Constructor
         SimpleXMLHandler(List<Person> people) {
             this.people = people;
         }
 
-
+        // Called when an opening XML tag (e.g., <person>) is encountered
         @Override
         public void startElement(String uri, String localName, String qName,
                 Attributes attributes) {
@@ -43,11 +47,21 @@ public class SimpleXMLReader {
             }
         }
 
+        // Called when a closing XML tag (e.g., </person>) us encountered
         @Override
         public void endElement(String uri, String localName, String qName) {
-            // student to add
+//             student to add
+            if (qName.equalsIgnoreCase("person")) {
+                people.add(Person.fromMap(current));
+                current = null;
+            } else {
+                if (current != null) {
+                    current.put(qName, buffer.toString());
+                }
+            }
         }
 
+        // Called when character data (text between XML tags) is encountered.
         @Override
         public void characters(char[] ch, int start, int length) {
             buffer.append(ch, start, length);
